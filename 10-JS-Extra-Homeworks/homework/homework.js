848c433bca202b50c70b85bd6b4bc743568d84a6
Pierre <pierreluigui16@gmail.com>
2022-01-25 20:03:23 -0500

    Homework 10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,16 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var matriz = [];
+
+  var key = Object.keys(objeto);
+  var value = Object.values(objeto);
+
+  for(var i = 0; i < key.length; i++){
+    matriz.push([key[i], value[i]]);
+  }
+
+  return matriz;
 }
 
 
@@ -18,6 +28,17 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var object = {};
+
+  for(i = 0; i < string.length; i++){
+    if(object[string[i]] >= 1){
+      object[string[i]]++;
+    } else{
+      object[string[i]] = 1;
+    }
+  }
+
+  return object;
 }
 
 
@@ -26,6 +47,18 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayus = "";
+  var minus = "";
+
+  for(var i = 0; i < s.length; i++){
+    if(s[i] === s[i].toUpperCase() && s[i] !== s[i].toLowerCase()){
+      mayus = mayus + s[i];
+    } else {
+      minus = minus + s[i];
+    }
+  }
+
+  return (mayus + minus);
 }
 
 
@@ -35,6 +68,32 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var arrayLetra = [];
+  var temp = "";
+  var fraseInv = "";
+
+  for(var i = 0; i < str.length; i++){
+    if(str[i] !== " "){
+      temp = temp + str[i];
+    }
+
+    if(str[i] === " " || i === str.length - 1){
+      arrayLetra.push(temp);
+      temp = "";
+    }
+  }
+
+  for(var i = 0; i < arrayLetra.length; i++){
+    for(var x = arrayLetra[i].length - 1; x >= 0; x--){
+      fraseInv = fraseInv + arrayLetra[i][x];
+    }
+
+    if(i !== arrayLetra.length - 1){
+      fraseInv = fraseInv + " ";
+    }
+  }
+
+  return fraseInv;
 } 
 
 
@@ -43,6 +102,18 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var count = 0;
+  numero = numero + "";
+  for(var i = 0; i < numero.length/2; i++){
+    if(numero[i] === numero[numero.length-1-i]){
+      count++;
+    }
+  }
+  if(count === Math.ceil(numero.length/2)){
+    return("Es capicua");
+  } else {
+    return("No es capicua");
+  }
 }
 
 
@@ -50,6 +121,18 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var temp = "";
+
+  for(var i = 0; i < cadena.length; i++){
+    if(cadena[i] !== "a"){
+      if(cadena[i] !== "b"){
+        if(cadena[i] !== "c"){
+          temp += cadena[i];
+        }
+      }
+    }
+  }
+  return temp;
 }
 
 
@@ -57,6 +140,11 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  function compare(a,b){
+    return a.length - b.length;
+  }
+  arr.sort(compare);
+  return arr;
 }
 
 
@@ -66,6 +154,15 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var array = [];
+  for(var i = 0; i < arreglo1.length; i++){
+    for(var j = 0; j < arreglo2.length; j++){
+      if(arreglo1[i] === arreglo2[j]){
+        array.push(arreglo2[j])
+      }
+    }
+  }
+  return array;
 }
 
 
